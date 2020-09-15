868176782b346f960d2dc0bad4a64da578709438
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2020-09-14 22:07:18 -0500

    Dev 1.1

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,38 @@
 //Write a Java program that prompts the user for a decimal number and then displays to the screen the binary equivalent as a String. 
 import java.util.Scanner;
 
-public class Driver
+public class Driver //initializes Driver class
 {
-	public static void main(String[] args)
+	public static void main(String[] args) //begins main method
 	{
-		System.out.println("The purpose of this program will be to convert decimal numbers that are inputed by the user into binary numbers");
-		System.out.println("[End Program]");
-	}
-}
+		//input value to be checked using scanner class
+		Scanner decInput = new Scanner(System.in); //opens the scanner
+		Integer startingInt = 0; //create variable to use inputed string after converted to int later
+		
+		while(true)
+		{
+			System.out.println("Please enter the number you wish to convert to binary: ");
+			String initialString = decInput.nextLine(); //takes value inputed and assigns value to a variable.
+			
+			try //this code will be executed if possible unless input is not proper type
+			{
+				startingInt = Integer.parseInt(initialString); //convert the string to an integer to be used later in program
+				decInput.close(); //close scanner resource leak
+				break;
+			}
+			catch(Exception a) //if code above fails in line 17-22 this will be executed.
+			{
+				System.out.println("You must enter a number.");
+			}
+		} //end while loop
+		
+		//initialize variables
+		
+		
+		//debug output
+		System.out.println(startingInt);
+		
+		//------------ Keep all main method code above this line ------------
+		System.out.println("[End]"); //informs user that the program has ended
+	} //ends main method
+} //ends driver class
