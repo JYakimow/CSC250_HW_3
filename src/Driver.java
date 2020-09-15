07e02305b91d812bde0f24f2d43db53a86de1448
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2020-09-14 19:53:45 -0500

    Dev 1.0

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+//Write a Java program that prompts the user for a decimal number and then displays to the screen the binary equivalent as a String. 
+import java.util.Scanner;
+
+public class Driver
+{
+	public static void main(String[] args)
+	{
+		System.out.println("The purpose of this program will be to convert decimal numbers that are inputed by the user into binary numbers");
+		System.out.println("[End Program]");
+	}
+}
